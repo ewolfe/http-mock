75120c7aa1c48752e4ed593accb56970d38fc6dc
ewolfe <e@ewolfe.me>
2021-03-23 19:40:44 -0700

    Fix check for direct page load

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ module.exports = async (req, res) => {
         res.status(204).send("");
         return;
       default:
-        if (req.headers.origin && req.headers.origin.endsWith("httpmock.app")) {
+        if (!req.headers.origin && !req.headers.referer) {
           res.setHeader("Content-Type", "text/html; charset=UTF-8");
           res.send(
             '<p>Please see <a href="https://github.com/ewolfe/http-mock">https://github.com/ewolfe/http-mock</a> to use httpmock.app programatically.</p>'
